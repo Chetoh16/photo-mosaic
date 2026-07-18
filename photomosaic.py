0d4d5d46cce13d7eaadfe6eea6dcea10151a1a63
Chetoh16 <egecetin1511@gmail.com>
2026-07-18 21:50:56 +0100

    feat: add boilerplate code for functions

--- photomosaic.py
+++ photomosaic.py
@@ -1,2 +1,30 @@
 from PIL import Image
-im = Image.open("assets/alex-ege-pics/SS853344.JPG")
+input_image = Image.open("assets/alex-ege-pics/SS853344.JPG")
+
+#print(input_image.format, input_image.size, input_image.mode)
+
+# to turn blue
+# r, g, b = im.split()
+# im = Image.merge("RGB", (b, g, r))
+
+def convert_into_grid(self):
+    pass
+
+def get_average_colour_value(self, image):
+    pass
+
+def pixelate_image(self, iamge):
+    pass
+
+def crop_images_into_squares(self, images):
+    pass
+
+def build_mosaic_image(self, images):
+    pass
+
+
+input_image.show()
+
+
+
+
