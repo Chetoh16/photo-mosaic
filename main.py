6ec03a2d12b21ebca145830cffe79ccb05c1bb87
Chetoh16 <egecetin1511@gmail.com>
2026-07-19 03:52:29 +0100

    chore: add rgb cache

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from photomosaic import *
 if __name__ == "__main__":
 
     # image chosen to be mosaic-ed
-    input_image_path = "assets/alex-ege-pics/SS853566.JPG"
+    input_image_path = "assets/alex-ege-pics/SS853599.JPG"
 
     # directory of source images (should be squares)
     source_images_dir_path = "assets/square-source-pics"
